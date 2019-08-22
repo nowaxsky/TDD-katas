df1eec0d7ac88f96e699d3d6572f6e679bd50035
andy-chuang <Andy_Chuang@trendmicro.com>
2019-08-22 11:57:40 +0800

    Workshop

--- src/main/java/chuck/tennis/TennisGameImpl.java
+++ src/main/java/chuck/tennis/TennisGameImpl.java
@@ -1,17 +1,45 @@
 package chuck.tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisGameImpl implements TennisGame {
 
-  int isWonPointCalled;
+    private final static int DEUCE = 6;
+    private final static String DASH = "-";
+    private final static String ALL = "All";
+    private Map<Integer, String> scoreMap = new HashMap<>();
 
-  @Override
-  public void wonPoint(String player) {
-    isWonPointCalled++;
-  }
+    private int Andy = 0;
+    private int Chuck = 0;
 
-  @Override
-  public String getScore() {
-    if(isWonPointCalled >0) return "Fifteen-Love";
-    return "Love-All";
-  }
+    TennisGameImpl() {
+        scoreMap.put(0, "Love");
+        scoreMap.put(1, "Fifteen");
+        scoreMap.put(2, "Thirty");
+        scoreMap.put(3, "Forty");
+    }
+
+    @Override
+    public void wonPoint(String player) {
+        if ("Andy".equals(player)) {
+            Andy++;
+        } else {
+            Chuck++;
+        }
+    }
+
+    private int getTotal() {
+        return Andy + Chuck;
+    }
+
+    @Override
+    public String getScore() {
+        if (getTotal() == 10) return "Win for Chuck";
+        else if (getTotal() == 7) return "Advantage Andy";
+        else if (getTotal() >= DEUCE) return "Deuce";
+        else if (getTotal() == 0) return scoreMap.get(0) + DASH + ALL;
+        else return scoreMap.get(Andy) + DASH + scoreMap.get(Chuck);
+    }
+
 }
--- src/test/java/chuck/tennis/TennisGameTests.java
+++ src/test/java/chuck/tennis/TennisGameTests.java
@@ -24,4 +24,97 @@ public class TennisGameTests {
     game.wonPoint("Andy");
     assertEquals("Fifteen-Love",game.getScore());
   }
+
+  @Test
+  public void getScore_2to0_ThirtyLove() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    assertEquals("Thirty-Love",game.getScore());
+  }
+
+  @Test
+  public void getScore_3to3_Deuce() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    assertEquals("Deuce",game.getScore());
+  }
+
+  @Test
+  public void getScore_4to4_Deuce() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    assertEquals("Deuce",game.getScore());
+  }
+  @Test
+  public void getScore_3to0_FortyLove() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    assertEquals("Forty-Love",game.getScore());
+  }
+
+  @Test
+  public void getScore_3to2_FortyThirty() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    assertEquals("Forty-Thirty",game.getScore());
+  }
+
+  @Test
+  public void getScore_2to3_ThirtyForty() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    assertEquals("Thirty-Forty",game.getScore());
+  }
+
+  @Test
+  public void getScore_3to1_FortyFifteen() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    assertEquals("Forty-Fifteen",game.getScore());
+  }
+
+  @Test
+  public void getScore_4to3_AdvantageAndy() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Andy");
+    assertEquals("Advantage Andy",game.getScore());
+  }
+  @Test
+  public void getScore_4to6_WinForChuck() {
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Andy");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    game.wonPoint("Chuck");
+    assertEquals("Win for Chuck",game.getScore());
+  }
 }
